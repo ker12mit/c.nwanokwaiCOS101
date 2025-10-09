19845392bf673f9d7bb0329cdcf32c85d32fc6da
ker12mit <chukwudi.nwanokwai@pau.edu.ng>
2025-10-09 15:11:02 +0100

    Uploaded practice 1 - 4

---
+++ week-2/pratice_1.rs
@@ -0,0 +1,20 @@
+ fn main() {
+	println!("welcome to cos 101!");
+
+	println!("The course learning outcome is as follows");
+
+	println!("1. distinguish between computer programming,\n
+			2. develop some techniques in computer science,\n
+			3. conversant with the applications of computer science,\n
+			4. navigate the career prospect in computer science,\n
+			5.conversant with computer programming concepts,\n"
+
+			);
+
+
+
+
+
+}
+
+
