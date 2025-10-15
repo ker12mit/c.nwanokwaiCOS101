4225d4f58ebd9c8f998a4a0fc16713f14fb0a7f2
ker12mit <chukwudi.nwanokwai@pau.edu.ng>
2025-10-15 17:12:27 +0100

    practice week-3

---
+++ week-3/practice_2/target/debug/practice_2.d
@@ -0,0 +1,1 @@
+C:\Users\New\Documents\c.nwanokwaiCOS101\week-3\practice_2\target\debug\practice_2.exe: C:\Users\New\Documents\c.nwanokwaiCOS101\week-3\practice_2\src\main.rs
---
+++ week-3/practice_9/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let mut fees = 25_000;
+    println!("fees are{}",fees );
+
+    fees = 35_000;
+    println!("fees changed is{}",fees );
+
+}
